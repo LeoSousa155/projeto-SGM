7ca4a74f7e4c74920345f34ebcca5eb46a47df96
LeoSousa155 <leonardodavid.dev@gmail.com>
2025-11-25 15:44:51 +0000

    feat: criação da cena do barco

---
+++ SingleplayerPlaying.java
@@ -0,0 +1,42 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class SingleplayerPlaying here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class SingleplayerPlaying extends World
+{
+    private static GreenfootImage bg = new GreenfootImage("background.jpg");
+    
+    static {
+        bg.scale(800, 600);
+    }
+    
+    @Override
+    public void started()
+    {
+        // We are in a world where menu music should be playing
+        MusicManager.enableMenuMusic();
+        MusicManager.onScenarioStarted();
+    }
+
+    @Override
+    public void stopped()
+    {
+        // Scenario paused/stopped from this world
+        MusicManager.onScenarioStopped();
+    }
+    
+    public SingleplayerPlaying()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(bg.getWidth(), bg.getHeight(), 1); 
+        setBackground(bg);
+        
+        // Adiciona o barco ao centro do mundo
+        Boat boat = new Boat();
+        addObject(boat, bg.getWidth() / 2, bg.getHeight() / 2);
+    }
+}
